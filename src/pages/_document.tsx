d9454703b2145122799c02b0801da9b0334b1743
zlvsky <zlvsky@icloud.com>
2023-12-11 09:44:35 +0100

    feat: added gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Main, Head, Html, NextScript, DocumentContext } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -51,11 +52,39 @@ class MyDocument extends Document {
             type="font/ttf"
             crossOrigin=""
           />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
           <link rel="manifest" href="./site.webmanifest" />
           {/* <link rel="alternate" title="rss feed" type="application/rss+xml" href="/rss.xml" /> */}
+
+          <Script
+            strategy="lazyOnload"
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+          ></Script>
+          <Script strategy="lazyOnload" id="gtagLoad">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.GA_TRACKING_ID}');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
