9a6d31baf39e60647f950aaab6bd7c0a5fd493af
zlvsky <zlvsky@icloud.com>
2024-01-23 22:20:04 +0100

    node.js free deploying on vercel post

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,15 +1,16 @@
 import { Category } from "@/types/Posts";
 
 export const categoriesArr: Category[] = [
-  { title: "TypeScript", path: "/kategorie/typescript" },
-  { title: "JavaScript", path: "/kategorie/javascript" },
+  // { title: "TypeScript", path: "/kategorie/typescript" },
+  // { title: "JavaScript", path: "/kategorie/javascript" },
   { title: "React", path: "/kategorie/react" },
-  { title: "HTML", path: "/kategorie/html" },
-  { title: "CSS", path: "/kategorie/css" },
-  { title: "PIXI.js", path: "/kategorie/dostepnosc" },
-  { title: "Design", path: "/kategorie/design" },
-  { title: "Firebase", path: "/kategorie/firebase" },
-  { title: "Praca", path: "/kategorie/praca" },
-  { title: "Architektura", path: "/kategorie/architektura" },
+  { title: "NodeJs", path: "/kategorie/nodejs" },
+  // { title: "HTML", path: "/kategorie/html" },
+  // { title: "CSS", path: "/kategorie/css" },
+  // { title: "Design", path: "/kategorie/design" },
+  // { title: "PIXI.js", path: "/kategorie/dostepnosc" },
+  // { title: "Firebase", path: "/kategorie/firebase" },
+  // { title: "Praca", path: "/kategorie/praca" },
+  // { title: "Architektura", path: "/kategorie/architektura" },
   { title: "Inne", path: "/kategorie/inne" },
 ];
